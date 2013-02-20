369040a9b58a2d8c06089a5311a6d3a338e39130
B31178 <travisp@fullsail.edu>
2013-02-20 17:34:06 -0500

    Second review submission.

--- pochintesta_travis_assignment3.js
+++ pochintesta_travis_assignment3.js
@@ -12,52 +12,67 @@
 //  Objects
 
 var carOne = {
-	"gallons": 15, /*Property Number*/
-	"miles": 250, /*Property Number*/
+
+	"price": 15000, /*Property Number*/
 	"color": "black", /*Property String*/
-	"monoChrome": true /*Property Boolean*/
-	"average": function () { /*Method Function*/
-				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
+	"monoChrome": true, /*Property Boolean*/
+	"distance": function(gallons, mpg) { /*Method Function*/
+				var outcome = (gallons * mpg); /*Math*/
 				return outcome;
 				}
-	
-	}
+			};
+
 
 var carTwo = { /*Contains Mutator*/
 
-	"gallons": 10, /*Property Number*/
-	"miles": 300, /*Property Number*/
+	"price": 10000, /*Property Number*/
 	"color": "red", /*Property Number*/
-	"monoChrome": false /*Property Boolean*/
-	"average": function () { /*Method Function*/
-				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
+	"monoChrome": false, /*Property Boolean*/
+	"distance": function(gallons, mpg) { /*Method Function*/
+				var outcome = (gallons * mpg); /*Math*/
 				return outcome;
 				},
 				
-	"altColor": function (newColor) { /*Method Mutator*/
+	"altColor": function(newColor) { /*Method Mutator*/
 				this.color = newColor;
-				}
-	if this.color === "white" { /*Procedure*/
-		this.monochrome === true
-		} else { this.monochrome === false
-		}
-	}
+				},
+				
+	if (this.color === "white") { /*Procedure*/
+			this.monochrome = true;
+			console.log("Yes!");
+		} else { 
+			this.monochrome = false;
+			console.log("No.");
+		},
+		
+}
 
 var carThree = {
 
-	"gallons": 12, /*Property Number*/
-	"miles": 350, /*Property Number*/
-	"color": ["white", "gray", "black"] /*Property Array of Strings*/
-	"monoChrome": true /*Property Boolean*/
-	"average": function () { /*Method Function*/
-				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
+	"price": 12000, /*Property Number*/
+	"color": ["white", "gray", "black"], /*Property Array of Strings*/
+	"monoChrome": true, /*Property Boolean*/
+	"distance": function(gallons, mpg) { /*Method Function*/
+				var outcome = (gallons * mpg); /*Math*/
 				return outcome;
 				}
-	
 	}
 	
 //  Main Code
 
-console.log("The first car's gas mileage is " + carOne.average());
+console.log("I'm looking for an affordable monochromatic car that can go the furthest on 5 gallons of gas.");
+console.log("There are 3 models in particular that I've been considering.");
 
-carTwo.newColor("white");
+console.log("The first car costs " + carOne.price + " and is " + carOne.color + ".");
+console.log("It can travel " + carOne.distance(5, 25) + " on 5 gallons of gas.");
+
+console.log("The second car costs " + carTwo.price + " and is " + carOne.color + ".");
+console.log("It can travel " + carOne.distance(5, 28) + " on 5 gallons of gas.");
+
+console.log("The third car costs " + carThree.price + " and is " + carOne.color + ".");
+console.log("It can travel " + carOne.distance(5, 30) + " on 5 gallons of gas.");
+
+console.log("I'm leaning towards purchasing the second car since it costs the least and has decent gas mileage.");
+console.log("The problem is it only comes in " + carTwo.color + ".")
+console.log("Is this car available in white?");
+carTwo.altColor("white");
