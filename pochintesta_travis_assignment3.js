c7b628c64459e286184aef49c6ad428b87fc6b73
B31178 <travisp@fullsail.edu>
2013-02-20 15:10:48 -0500

    First review submission.

--- pochintesta_travis_assignment3.js
+++ pochintesta_travis_assignment3.js
@@ -6,21 +6,18 @@
 //  Global Variables
 
 //  Arrays
-/*Array of Objects*/ [carOne, carTwo, carThree]
 
 //  Functions
-
-/*Average*/
-
 
 //  Objects
 
 var carOne = {
-	"gallons": 15,
-	"miles": 250,
-	"color": "black",
-	"average": function () {
-				var outcome = (this.gallons * this.miles) / 2;
+	"gallons": 15, /*Property Number*/
+	"miles": 250, /*Property Number*/
+	"color": "black", /*Property String*/
+	"monoChrome": true /*Property Boolean*/
+	"average": function () { /*Method Function*/
+				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
 				return outcome;
 				}
 	
@@ -28,33 +25,39 @@ var carOne = {
 
 var carTwo = { /*Contains Mutator*/
 
-	"gallons": 10,
-	"miles": 300,
-	"color": "red",
-	"average": function () {
-				var outcome = (this.gallons * this.miles) / 2;
+	"gallons": 10, /*Property Number*/
+	"miles": 300, /*Property Number*/
+	"color": "red", /*Property Number*/
+	"monoChrome": false /*Property Boolean*/
+	"average": function () { /*Method Function*/
+				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
 				return outcome;
 				},
 				
-	"altColor": function (newColor) {
+	"altColor": function (newColor) { /*Method Mutator*/
 				this.color = newColor;
 				}
-	
+	if this.color === "white" { /*Procedure*/
+		this.monochrome === true
+		} else { this.monochrome === false
+		}
 	}
 
 var carThree = {
 
-	"gallons": .75,
-	"miles"; 2,
-	"color": 1.5,
-	"average": function () {
-				var outcome = (this.gallons * this.miles) / 2;
+	"gallons": 12, /*Property Number*/
+	"miles": 350, /*Property Number*/
+	"color": ["white", "gray", "black"] /*Property Array of Strings*/
+	"monoChrome": true /*Property Boolean*/
+	"average": function () { /*Method Function*/
+				var outcome = (this.gallons * this.miles) / 2; /*Method Accessor & Math*/
 				return outcome;
 				}
 	
 	}
+	
+//  Main Code
 
+console.log("The first car's gas mileage is " + carOne.average());
 
-
-
-
+carTwo.newColor("white");
